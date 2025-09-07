f13157209bb9bd5312b336b8ab6d572b0bbd8a15
andiazl <anyel.123.diaz@gmail.com>
2025-09-07 04:05:55 -0500

    (caso3): crear clase CuentaBancaria con atributos y constructor

---
+++ src/caso3/CuentaBancaria.java
@@ -0,0 +1,14 @@
+package caso3;
+
+import java.util.Scanner;
+
+public class CuentaBancaria {
+	
+	private double saldo;
+
+    // Constructor inicializando el saldo
+    public CuentaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+    
+}
