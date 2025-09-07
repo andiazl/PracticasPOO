ef2d1ca1aea2388faf2bd89fed4d57cac2fd1fc5
andiazl <anyel.123.diaz@gmail.com>
2025-09-07 05:31:58 -0500

    Creacion de rama de desarrollo de rodrigo y pase de su solucion caso 1

---
+++ src/caso1/UsuarioSimple.java
@@ -0,0 +1,27 @@
+package caso1;
+
+import java.util.Scanner;
+
+public class UsuarioSimple {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        System.out.print("Ingrese su altura (en metros): ");
+        double altura = sc.nextDouble();
+
+        System.out.println("\n--- Datos ingresados ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Altura: " + altura + " m");
+
+        sc.close();
+    }
+	
+}
